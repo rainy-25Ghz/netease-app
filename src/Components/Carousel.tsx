f24aaa5f8b35d2505aec38230c638e8fd0cd430c
rainy25ghz <1748420480@qq.com>
2021-09-29 16:26:56 +0800

    1

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -4,12 +4,21 @@ import styled from "@emotion/styled";
 import { useBanner } from "../hooks/useBanner";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { CircularProgress } from "@mui/material";
 const StyledDiv = styled.div`
     height: 250px;
-    width:250px;
+    width: 250px;
 `;
-const StyledSlider = styled(Slider)`
+const StyledSlider = styled(Slider)``;
+interface ContainerProps {
+    img: string;
+}
+const StyledContainer = styled.div<ContainerProps>`
+    height: 250px;
+    width: 250px;
+    background-image: url(${(props) => props.img});
 `;
+
 interface Props {
     imgs?: string[];
 }
@@ -18,26 +27,21 @@ export const Carousel = (props: Settings & Props) => {
     const { bannerData } = useBanner();
     return (
         <StyledDiv>
-            <StyledSlider {...props}>
-                <div style={{backgroundColor:"black"}}>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </StyledSlider>
+            {bannerData ? (
+                <StyledSlider {...props}>
+                    {bannerData.banners.map((img: any) => {
+                        console.log(img);
+                        return (
+                            <StyledContainer
+                                key={img.imageUrl}
+                                img={img.imageUrl}
+                            ></StyledContainer>
+                        );
+                    })}
+                </StyledSlider>
+            ) : (
+                <CircularProgress />
+            )}
         </StyledDiv>
     );
 };
